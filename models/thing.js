8d039b7e3d8dbce4f856f071d2c2024fe91a60d2
marcel <uduma.matrix@gmail.com>
2021-05-16 18:18:41 +0100

    the first commit

---
+++ models/thing.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const thingsSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    discription: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    userId: {type: String, required: true},
+    price: {type: Number, required: true},
+}); 
+
+module.exports = mongoose.model('Thing', thingsSchema);
