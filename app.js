269f4d0dfbc25dce732aeb65ee9cdf29a551b3ad
marcel <uduma.matrix@gmail.com>
2021-05-21 19:18:00 +0100

    Connecting to momgoose DB atlass

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express =  require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
+// Template for connecting to the mongoose DB
+// mongoose.connect('mongodb+srv://username:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true')
+/**
+ * My Database connection
+ * Name: marcel
+ *  Password: DjNMoZBYRMRITW8I
+ * mongodb+srv://marcel:<password>@cluster0.2wopq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+ */
+
+mongoose.connect('mongodb+srv://marcel:DjNMoZBYRMRITW8I@cluster0.2wopq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
 
 
 app.use((req, res, next) => {
