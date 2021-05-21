15e58f5262d6fae26c3029d8c68bb324264034ed
marcel <uduma.matrix@gmail.com>
2021-05-21 16:00:26 +0100

    Just added the cors account

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express =  require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
         {
