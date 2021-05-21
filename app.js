03ca5a4a9803ced8b244e6f469e84900ec286a9f
marcel <uduma.matrix@gmail.com>
2021-05-21 15:53:45 +0100

    Making the first post end point /api/stuff  this one was created after seeing what the middle ware was

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express =  require('express');
 const app = express();
 
-app.use((req, res) => {
-    res.json({message: "Lorem ispum dat message"})
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+
+    res.status(201).json(stuff);
 })
 
 
