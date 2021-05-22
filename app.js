6bec758e0e0d1277394f8d972fc66f4f8faa6dc5
marcel <uduma.matrix@gmail.com>
2021-05-22 15:10:09 +0100

    Just added the last things in the crud being the update and the delete

--- app.js
+++ app.js
@@ -77,6 +77,50 @@ app.get('/api/stuff/:id', (req, res, next) => {
 })
 
 
+app.put('/api/stuff/:id', (req, res, next) => {
+    const thing = new Thing({
+         _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+
+    Thing.updateOne({
+        _id: req.params.id
+    }, thing).then(
+        () => {
+           res.status(201).json({
+               message : "Thing updated successgully"
+           })
+        }
+    ).catch(
+        error => {
+            res.status(400).json({
+                error: error
+            })
+        }
+    )
+
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({
+        _id: req.params.id
+    }).then(
+        thing => {
+            res.status(200).json({message: "Content deleted"})
+        }
+    ).catch(
+        error => {
+            res.status(404).json({
+                message: error
+            })
+        }
+    )
+})
+
 app.use('/api/stuff', (req, res, next) => {
   Thing.find().then(
       things => {
